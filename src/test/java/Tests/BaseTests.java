c67942ddee80c78f6c6072a90b6969ee8b274465
Abdelrhman-Ellithy <abdelarhmanellithy@gmail.com>
2025-03-15 16:59:15 +0200

    release 2.0.2

--- src/test/java/Tests/BaseTests.java
+++ src/test/java/Tests/BaseTests.java
@@ -2,24 +2,13 @@ package Tests;
 
 import Ellithium.Utilities.helpers.PropertyHelper;
 import Ellithium.core.driver.DriverFactory;
-import Ellithium.core.base.NonBDDSetup;
-import Ellithium.core.driver.DriverType;
 import Ellithium.core.driver.LocalDriverType;
 import Pages.HomPage;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.*;
-public class BaseTests extends NonBDDSetup {
+public class BaseTests {
     WebDriver driver;
     HomPage home;
-    @BeforeSuite
-    public void openReport(){
-        PropertyHelper.setDataToProperties("src/main/resources/properties/allure.properties",
-                "allure.generate.report",
-                "true");
-        PropertyHelper.setDataToProperties("src/main/resources/properties/allure.properties",
-                "allure.open.afterExecution",
-                "false");
-    }
     @BeforeClass
     public void Setup(){
         driver= DriverFactory.getNewLocalDriver(LocalDriverType.Chrome);
